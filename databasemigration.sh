e25e26d36bff18074d62f10c28dfa36dc4d5edb3
kashyapdivya93 <kashyapdivya93@gmail.com>
2020-07-17 20:16:52 +0530

    Update databasemigration.sh

--- databasemigration.sh
+++ databasemigration.sh
@@ -1,5 +1,5 @@
-BASTION_IP=150.136.9.124
-TARGET_DB_HOST=10.0.5.2
+BASTION_IP=
+TARGET_DB_HOST=
 scp -i ~/.ssh/privatekey  ~/wlsworkshop/WebLogic-to-oci/createSchema.sql opc@${BASTION_IP}:/home/opc/
 scp -i ~/.ssh/privatekey -o ProxyCommand="ssh -W %h:%p opc@${BASTION_IP} -i ~/.ssh/privatekey" -r ~/wlsworkshop/WebLogic-to-oci/createSchema.sql opc@${TARGET_DB_HOST}:/home/opc/
 ssh -i ~/.ssh/privatekey -o ProxyCommand="ssh -W %h:%p opc@${BASTION_IP} -i ~/.ssh/privatekey" opc@${TARGET_DB_HOST} "sudo chown -R oracle:oinstall /home/opc/createSchema.sql && sudo rm -rf /home/oracle/createSchema.sql && sudo mv /home/opc/createSchema.sql /home/oracle/"
