9fa2f4e1a9cf3b4bff84c1b2632cac393ba10052
divkashy <divkashy@DIVKASHY-IN.in.oracle.com>
2020-09-05 01:11:56 +0530

    update datamigration script

--- databasemigration.sh
+++ databasemigration.sh
@@ -1,6 +1,8 @@
 BASTION_IP=
 TARGET_DB_HOST=
-scp -i ~/.ssh/privatekey  ~/wlsworkshop/WebLogic-to-oci/createSchema.sql opc@${BASTION_IP}:/home/opc/
-scp -i ~/.ssh/privatekey -o ProxyCommand="ssh -W %h:%p opc@${BASTION_IP} -i ~/.ssh/privatekey" -r ~/wlsworkshop/WebLogic-to-oci/createSchema.sql opc@${TARGET_DB_HOST}:/home/opc/
-ssh -i ~/.ssh/privatekey -o ProxyCommand="ssh -W %h:%p opc@${BASTION_IP} -i ~/.ssh/privatekey" opc@${TARGET_DB_HOST} "sudo chown -R oracle:oinstall /home/opc/createSchema.sql && sudo rm -rf /home/oracle/createSchema.sql && sudo mv /home/opc/createSchema.sql /home/oracle/"
-ssh -i ~/.ssh/privatekey -o ProxyCommand="ssh -W %h:%p opc@${BASTION_IP} -i ~/.ssh/privatekey" opc@${TARGET_DB_HOST} "sudo ls -a && sudo su - oracle bash -c "sqlplus""
+scp -i ~/wlsworkshop/WebLogic-to-oci/Bob  ~/wlsworkshop/WebLogic-to-oci/Bob opc@${BASTION_IP}:/home/opc/.ssh/
+ssh -i ~/wlsworkshop/WebLogic-to-oci/Bob opc@${BASTION_IP} "sudo chmod 400 /home/opc/.ssh/Bob"
+scp -i ~/wlsworkshop/WebLogic-to-oci/Bob  ~/wlsworkshop/WebLogic-to-oci/createSchema.sql opc@${BASTION_IP}:/home/opc/
+scp -i ~/wlsworkshop/WebLogic-to-oci/Bob -o ProxyCommand="ssh -W %h:%p opc@${BASTION_IP} -i ~/.ssh/Bob" -r ~/wlsworkshop/WebLogic-to-oci/createSchema.sql opc@${TARGET_DB_HOST}:/home/opc/
+ssh -i ~/wlsworkshop/WebLogic-to-oci/Bob -o ProxyCommand="ssh -W %h:%p opc@${BASTION_IP} -i ~/.ssh/Bob" opc@${TARGET_DB_HOST} "sudo chown -R oracle:oinstall /home/opc/createSchema.sql && sudo rm -rf /home/oracle/createSchema.sql && sudo mv /home/opc/createSchema.sql /home/oracle/"
+ssh -i ~/wlsworkshop/WebLogic-to-oci/Bob -o ProxyCommand="ssh -W %h:%p opc@${BASTION_IP} -i ~/.ssh/Bob" opc@${TARGET_DB_HOST} "sudo ls -a && sudo su - oracle bash -c "sqlplus""
